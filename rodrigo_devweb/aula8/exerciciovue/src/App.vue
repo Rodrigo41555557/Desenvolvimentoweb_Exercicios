28eabb6aab544c8c4f3c3cc3ad2f768ed574f548
Rodrigo41555557 <rodrigoiag797@gmail.com>
2022-04-28 09:10:21 -0300

    teste vue primeira versão

--- rodrigo_devweb/aula8/exerciciovue/src/App.vue
+++ rodrigo_devweb/aula8/exerciciovue/src/App.vue
@@ -1,14 +1,11 @@
 <script>
-import oComponente from "@/components/oComponente.vue"
-import OComponente from "./components/oComponente.vue"
+import OComponente from "./components/oComponente.vue";
 export default {
-    components: (MeuComponente: oComponente),
-    components: { OComponente }
+    components: { OComponente },
 }
 </script>
 
 <template>
   <h1>ola</h1>
-  <OComponente titulo="fazer" />
-  <O-Componente />
+  <OComponente/>
 </template>
