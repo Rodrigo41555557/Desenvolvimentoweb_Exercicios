e932bdb9d41b9506723697009e1a74a45b43c2ff
Rodrigo41555557 <rodrigoiag797@gmail.com>
2022-05-26 08:26:12 -0300

    aula 11

---
+++ rodrigo_devweb/b-aula 11/vue-db/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  async mounted() {
+    try {
+      const result = await axios.get("http://localhost:3001/tasks");
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
+</script>
+<template>
+  <div class="about">
+    <h1>Tasks</h1>
+    <hr />
+    <h1>{{ tasks }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
